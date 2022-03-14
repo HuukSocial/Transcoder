dcd2868a870037425b83bba9d680e6ed555dc14c
linh.bui <linh.bui@ntq-solution.com.vn>
2022-03-14 11:31:35 +0700

    add maven publisher

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,12 +1,8 @@
-import io.deepmedia.tools.publisher.common.GithubScm
-import io.deepmedia.tools.publisher.common.License
-import io.deepmedia.tools.publisher.common.Release
-import io.deepmedia.tools.publisher.sonatype.Sonatype
-
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("io.deepmedia.tools.publisher")
+    id("maven-publish")
 }
 
 android {
@@ -19,6 +15,18 @@ android {
     buildTypes["release"].isMinifyEnabled = false
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "com.github.HuukSocial"
+                artifactId = "Transcoder"
+                version = "1.0"
+                from(components["release"])
+            }
+        }
+    }
+}
 
 dependencies {
     api("com.otaliastudios.opengl:egloo:0.6.1")
@@ -28,35 +36,4 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.4.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("org.mockito:mockito-android:2.28.2")
-}
-
-publisher {
-    project.description = "Accelerated video transcoding using Android MediaCodec API without native code (no LGPL/patent issues)."
-    project.artifact = "transcoder"
-    project.group = "com.otaliastudios"
-    project.url = "https://github.com/natario1/Transcoder"
-    project.scm = GithubScm("natario1", "Transcoder")
-    project.addLicense(License.APACHE_2_0)
-    project.addDeveloper("natario1", "mat.iavarone@gmail.com")
-    release.sources = Release.SOURCES_AUTO
-    release.docs = Release.DOCS_AUTO
-    release.version = "0.10.4"
-
-    directory()
-
-    sonatype {
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
-
-    sonatype("snapshot") {
-        repository = Sonatype.OSSRH_SNAPSHOT_1
-        release.version = "latest-SNAPSHOT"
-        auth.user = "SONATYPE_USER"
-        auth.password = "SONATYPE_PASSWORD"
-        signing.key = "SIGNING_KEY"
-        signing.password = "SIGNING_PASSWORD"
-    }
 }
